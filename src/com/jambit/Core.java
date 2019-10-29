b8c76e7f9103eb4bd3a8ab6c818922d94291af65
Alexander Neuhäuser <alexander.neuhaeuser@jambit.com>
2019-10-29 15:59:32 +0100

    add second encrypter

--- src/com/jambit/Core.java
+++ src/com/jambit/Core.java
@@ -3,5 +3,6 @@ package com.jambit;
 public class Core {
   public static void main(String[] args) {
     Encrypter encrypter = new Encrypter();
+    System.out.println(encrypter.encrypt("FINN ist Dumm","500:6"));
   }
 }
