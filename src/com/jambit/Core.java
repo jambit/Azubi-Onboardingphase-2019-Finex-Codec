e903a3ccba5fc38c213a8c7b8717a628a22c4b2e
Alexander Neuhäuser <alexander.neuhaeuser@jambit.com>
2019-10-29 08:32:40 +0100

    Java Documantation

--- src/com/jambit/Core.java
+++ src/com/jambit/Core.java
@@ -3,9 +3,6 @@ package com.jambit;
 public class Core {
     public static void main(String[] args) {
         Encrypter encrypter = new Encrypter();
-        System.out.println(encrypter.encrypt("Hallo123" ,10));
-        System.out.println(encrypter.getEncryptionKey());
-        encrypter.randomCharSet(202);
-        System.out.println(encrypter.encrypt("Hallo123", 10));
+
     }
 }
--- src/com/jambit/Encrypter.java
+++ src/com/jambit/Encrypter.java
@@ -1,24 +1,48 @@
 package com.jambit;
 
-import java.nio.charset.Charset;
 import java.util.Random;
 
+/**
+ * Encrypts messages with a key
+ */
 public class Encrypter {
-    private int encryptionKey = 20;
+    private int encryptionKey;
     private String charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖabcdefghijklmnopqrstuvwxyzäüöß0123456789,.!?\"§$%&/()=+-*\\_#~<>| ";
 
+
+    public int getEncryptionKey() {
+        return encryptionKey;
+    }
+
+    /**
+     * encrypts a String with a random key
+     * @param msg String to encrypt
+     * @return returns a encrypted String
+     */
     public String encrypt(String msg) {
         encryptionKey = (int) (Math.random() * charSet.length() - 1) + 1;
-        return encryptionAlgorithm(msg, encryptionKey);
+        return encryptionCaesarCipher(msg, encryptionKey);
     }
 
+    /**
+     * encrypts a String with a encryption Key
+     * @param msg String to encrypt
+     * @param encryptionKey Key you want to use
+     * @return returns a encrypted String
+     */
     public String encrypt(String msg, int encryptionKey) {
         this.encryptionKey = encryptionKey;
-        return encryptionAlgorithm(msg, encryptionKey);
+        return encryptionCaesarCipher(msg, encryptionKey);
     }
 
-    private String encryptionAlgorithm(String msg, int key) {
-        String encryptedMessage = "";
+    /**
+     * encrypts the message with Caesar cipher
+     * @param msg String to encrypt
+     * @param key key to use for the encrypt
+     * @return returns encrypted message
+     */
+    private String encryptionCaesarCipher(String msg, int key) {
+        StringBuilder encryptedMessage = new StringBuilder();
 
         for (int i = 0; i < msg.length(); i++) {
             int loc = charSet.indexOf(msg.charAt(i)) + key;
@@ -27,21 +51,17 @@ public class Encrypter {
                 loc -= charSet.length();
             }
 
-            encryptedMessage += charSet.charAt(loc);
+            encryptedMessage.append(charSet.charAt(loc));
         }
 
-        return encryptedMessage;
-    }
-
-    public int getEncryptionKey() {
-        return encryptionKey;
+        return encryptedMessage.toString();
     }
 
     public void randomCharSet(int seed) {
         Random generator = new Random(seed);
         for (int i = 1; i < generator.nextInt(1000); i++) {
             for (int j = 0; j < charSet.length(); j++) {
-                generator = new Random(seed + (i * (j + 1)));
+                generator = new Random(seed + (i * 3 * (j + 1)));
                 int randNum = generator.nextInt(charSet.length() - 1);
                 charSet = swapIndex(charSet, j, randNum);
             }
@@ -56,4 +76,28 @@ public class Encrypter {
         return sb.toString();
     }
 
+
+    /**
+     * Generates a Key
+     * @return returns the key as String
+     */
+    private String generateKey(){
+        Random generator = new Random();
+        String key = generator.nextInt() + ":" + generator.nextInt(charSet.length());
+        return key;
+    }
+
+    /**
+     * Spits a key into multiple parts
+     * @param key key you want to split
+     * @return a String array with all keys
+     */
+    private String[] splitKey(String key){
+        String[] keys = key.split("[:]");
+        if(keys.length < 2) {
+            return null;
+        }else{
+            return keys;
+        }
+    }
 }
