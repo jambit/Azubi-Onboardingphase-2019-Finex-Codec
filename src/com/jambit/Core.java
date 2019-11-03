668ee3a4a382e519b08976f81c5cff8d365e1e22
Alexander Neuhäuser <alexander.neuhaeuser@jambit.com>
2019-11-03 13:05:34 +0100

    rsa performance update

--- src/com/jambit/Core.java
+++ src/com/jambit/Core.java
@@ -1,5 +1,7 @@
 package com.jambit;
 
+import java.math.BigInteger;
+
 public class Core {
   public static void main(String[] args) throws Exception {
     UserInterface ui = new UserInterface();
@@ -13,8 +15,8 @@ public class Core {
      * encryptedMessage); System.out.println("Key: " + caesarCodec.getEncryptionKey());
      * ui.saveFileChooser(encryptedMessage);
      */
-    String[] x = rsaCodec.asymmetricKeyGenerator(80863, 67369);
+    String[] x = rsaCodec.asymmetricKeyGenerator("62596183837793660881747092379034939976984370076262238741250390581643860136423", "57,280,268,856,223,100,619,507,809,781,996,738,021,850,347,661,097,823,339,843,797,463,812,950,192,909");
     System.out.println("keys \n" + x[0] + "\n" + x[1]);
-    rsaCodec.RSADecoder(rsaCodec.RCAEncrypt("Helmut", x[0]), x[1]);
+    rsaCodec.RSADecoder(rsaCodec.RCAEncrypt("hallo das ist ein test ", x[0]), x[1]);
   }
 }
--- src/com/jambit/RSACodec.java
+++ src/com/jambit/RSACodec.java
@@ -4,85 +4,76 @@ import java.math.BigInteger;
 
 public class RSACodec {
 
-  public String RCAEncrypt(String msg, String key) {
-    String out = "";
-    String[] keys = CaesarCodec.splitKey(key);
-    for (int i = 0; i < msg.length(); i++) {
-      long index = CaesarCodec.charSet.indexOf(msg.charAt(i)) + 1;
-      BigInteger bi = new BigInteger(index + "");
-      bi = bi.modPow(new BigInteger(keys[0]), new BigInteger(keys[1]));
-      String x;
-      if (bi.longValue() == 0) {
-        x = bi.longValue() + "";
-      } else {
-        x = ((bi.longValue() - 1) + "");
-      }
-      out += x + " ";
+    public BigInteger primeNumber(int bitSize){
+        long bs =(long)Math.pow(2,bitSize);
+        new BigInteger(bs +"");
+        return null;
     }
-    System.out.println(out);
-    return out;
-  }
 
-  public void RSADecoder(String msg, String key) {
-    String out = "";
-    String[] vars = msg.split(" ");
-
-    String[] keys = CaesarCodec.splitKey(key);
-    for (int i = 0; i < vars.length; i++) {
-      long index = Long.parseLong(vars[i]) + 1;
-      BigInteger bi = new BigInteger(index + "");
-      bi = bi.modPow(new BigInteger(keys[0]), new BigInteger(keys[1]));
-      String x;
-      if (bi.longValue() == 0) {
-        x = CaesarCodec.charSet.charAt(bi.intValue()) + "";
-      } else {
-        x = CaesarCodec.charSet.charAt(bi.intValue() - 1) + "";
-      }
-      out += x;
-    }
-    System.out.println(out);
-  }
-
-  public String[] asymmetricKeyGenerator(long p1, long p2) {
-    String[] keys = {"", ""};
-    long N = p1 * p2;
-    long o = (p1 - 1) * (p2 - 1);
-    long e = commonFactors(N, o);
-    long i = 1;
-    long d;
-    int j = 1;
-    long first = 0;
-    while (true) {
-      if ((e * i) % o == 1) {
-        j++;
-        d = i;
-        if (first == 0) {
-          first = d;
-        } else {
-          i = j * first;
+    public String RCAEncrypt(String msg, String key) {
+        String out = "";
+        String[] keys = CaesarCodec.splitKey(key);
+        for (int i = 0; i < msg.length(); i++) {
+            long index = CaesarCodec.charSet.indexOf(msg.charAt(i)) + 1;
+            BigInteger bi = new BigInteger(index + "");
+            bi = bi.modPow(new BigInteger(keys[0]), new BigInteger(keys[1]));
+            String x;
+            if (bi.longValue() == 0) {
+                x = bi.longValue() + "";
+            } else {
+                x = ((bi.longValue() - 1) + "");
+            }
+            out += x + " ";
         }
-        System.out.println(d + "/" + e);
-        if (j > 2) {
-          break;
-        }
-      }
-      i += 2;
+        System.out.println(out);
+        return out;
     }
-    keys[0] = e + ":" + N;
-    keys[1] = d + ":" + N;
-    return keys;
-  }
 
-  private long commonFactors(long factor, long o) {
-    long out = 0;
-    for (long i = 1; i < o; i++) {
-      if (factor % i != 0 && o % i != 0 && i % 2 != 0) {
-        out = i;
-        if (Math.round(Math.random() * 100) > 1) {
-          break;
+    public void RSADecoder(String msg, String key) {
+        String out = "";
+        String[] vars = msg.split(" ");
+
+        String[] keys = CaesarCodec.splitKey(key);
+        for (int i = 0; i < vars.length; i++) {
+            BigInteger bi = new BigInteger(vars[i]).add(BigInteger.ONE);
+            bi = bi.modPow(new BigInteger(keys[0]), new BigInteger(keys[1]));
+            String x;
+            if (bi.longValue() == 0) {
+                x = CaesarCodec.charSet.charAt(bi.intValue()) + "";
+            } else {
+                x = CaesarCodec.charSet.charAt(bi.intValue() - 1) + "";
+            }
+            out += x;
         }
-      }
+        System.out.println(out);
     }
-    return out;
-  }
+
+    public String[] asymmetricKeyGenerator(String _p1, String _p2) {
+        _p1 = _p1.replace(",", "");
+        _p2 = _p2.replace(",", "");
+        BigInteger p1 = new BigInteger(_p1);
+        BigInteger p2 = new BigInteger(_p2);
+        String[] keys = {"", ""};
+        BigInteger N = p1.multiply(p2);
+        BigInteger o = p1.subtract(BigInteger.ONE).multiply(p2.subtract(BigInteger.ONE));
+        System.out.println(p1.gcd(p2) + "|" + o);
+        BigInteger e = coPrimeFactors(N, o);
+        System.out.println(e);
+        BigInteger d = (e.modInverse(o));
+        d = d.add(o);
+        keys[0] = e + ":" + N;
+        keys[1] = d + ":" + N;
+        return keys;
+    }
+
+    private BigInteger coPrimeFactors(BigInteger factor, BigInteger o) {
+        BigInteger out = null;
+        for (BigInteger i = new BigInteger("1"); i.compareTo(o) < 0; i = i.add(new BigInteger("1"))) {
+            if (!i.mod(new BigInteger("2")).equals(new BigInteger("0")) && !factor.mod(i).equals(new BigInteger("0")) && !o.mod(i).equals(new BigInteger("0"))) {
+                out = i;
+                break;
+            }
+        }
+        return out;
+    }
 }
