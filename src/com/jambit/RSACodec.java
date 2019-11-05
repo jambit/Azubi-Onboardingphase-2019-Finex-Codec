04b1b9cb0612b0ccf27bcefb98f7da2d531940c6
Alexander Neuhäuser <alexander.neuhaeuser@jambit.com>
2019-11-05 21:29:06 +0100

    encryption multithreading

--- src/com/jambit/RSACodec.java
+++ src/com/jambit/RSACodec.java
@@ -1,46 +1,58 @@
 package com.jambit;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class RSACodec {
 
-  //  public BigInteger primeNumberGenerator(int bitSize) {
-  //    String number = "";
-  //    for (int i = 0; i < bitSize; i++) {
-  //      number += "9";
-  //    }
-  //    BigInteger bi = new BigInteger(number);
-  //    while (!isPrime(bi)) {
-  //      bi.add(BigInteger.ONE);
-  //    }
-  //    return bi;
-  //  }
+  private final int CHAR_PER_THREAD = 20;
 
-  public String RSAEncrypt(String msg, String key) {
-    String out = "";
+  public String RSAEncrypt(String message, String key) {
+    StringBuilder encryptedMessage = new StringBuilder();
     String[] keys = CaesarCodec.splitKey(key);
-    byte[] base64ByteArrayKey1 = Base64.getDecoder().decode(keys[0]);
-    byte[] base64ByteArrayKey2 = Base64.getDecoder().decode(keys[1]);
-    keys[0] = new String(base64ByteArrayKey1);
-    keys[1] = new String(base64ByteArrayKey2);
-    for (int i = 0; i < msg.length(); i++) {
-      long index = CaesarCodec.charSet.indexOf(msg.charAt(i)) + 1;
-      BigInteger bi = new BigInteger(index + "");
-      bi = bi.modPow(new BigInteger(keys[0]), new BigInteger(keys[1]));
-      String x;
-      if (bi.longValue() == 0) {
-        x = bi.longValue() + "";
-      } else {
-        x = ((bi.longValue() - 1) + "");
-      }
-      out += Base64.getEncoder().encodeToString(x.getBytes()) + " ";
+
+    ArrayList<Encrypt> activeThreads = new ArrayList<Encrypt>();
+    int threadCount = message.length() / CHAR_PER_THREAD;
+    if (threadCount == 0) threadCount = 1;
+
+    // decodes keys
+    keys[0] = new String(Base64.getDecoder().decode(keys[0]));
+    keys[1] = new String(Base64.getDecoder().decode(keys[1]));
+
+    // adds n+1 new thread when the message cant be divided equally
+    int threadMessageLength = message.length() / threadCount;
+    if (message.length() % threadCount != 0) {
+      threadCount++;
     }
-    return out;
+    // Create Threads
+    for (int i = 0; i < threadCount; i++) {
+      int startCharIndex = i * threadMessageLength;
+      int endCharIndex = (i + 1) * threadMessageLength;
+
+      if (endCharIndex >= message.length()) {
+        endCharIndex = message.length();
+      }
+
+      activeThreads.add(new Encrypt(message.substring(startCharIndex, endCharIndex), keys));
+      activeThreads.get(i).start();
+    }
+
+    // Join threads
+    for (int i = 0; i < threadCount; i++) {
+      try {
+        activeThreads.get(i).join();
+        encryptedMessage.append(activeThreads.get(i).out);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return encryptedMessage.toString();
   }
 
   public String RSADecrypt(String msg, String key) {
-    String out = "";
+    StringBuilder out = new StringBuilder();
     String[] vars = msg.split(" ");
     for (int i = 0; i < vars.length; i++) {
       vars[i] = new String(Base64.getDecoder().decode(vars[i]));
@@ -61,9 +73,9 @@ public class RSACodec {
       } else {
         x = CaesarCodec.charSet.charAt(bi.intValue() - 1) + "";
       }
-      out += x;
+      out.append(x);
     }
-    return out;
+    return out.toString();
   }
 
   public String[] asymmetricKeyGenerator(String _p1, String _p2) {
@@ -101,18 +113,32 @@ public class RSACodec {
     return out;
   }
 
-  //  private boolean isPrime(BigInteger num) {
-  //    for (BigInteger divider = BigInteger.ONE;
-  //        num.compareTo(divider) > 0;
-  //        divider = divider.add(BigInteger.TWO)) {
-  //      if (!divider.equals(num) && !divider.equals(BigInteger.ONE)) {
-  //        if (num.mod(divider).equals(BigInteger.ZERO)
-  //            || num.sqrt().compareTo(divider) >= 0
-  //            || num.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
-  //          return false;
-  //        }
-  //      }
-  //    }
-  //    return true;
-  //  }
+  private class Encrypt extends Thread {
+    String msg;
+    String[] keys;
+    public StringBuilder out = new StringBuilder();
+
+    public Encrypt(String msg, String[] keys) {
+      this.msg = msg;
+      this.keys = keys;
+    }
+
+    @Override
+    public void run() {
+      BigInteger bi;
+      for (int i = 0; i < msg.length(); i++) {
+        bi =
+            new BigInteger((CaesarCodec.charSet.indexOf(msg.charAt(i)) + 1) + "")
+                .modPow(new BigInteger(keys[0]), new BigInteger(keys[1]));
+        if (bi.longValue() == 0) {
+          out.append(Base64.getEncoder().encodeToString((bi + "").getBytes())).append(" ");
+        } else {
+          out.append(
+                  Base64.getEncoder().encodeToString((bi.subtract(BigInteger.ONE) + "").getBytes()))
+              .append(" ");
+        }
+      }
+      System.out.println(this.getName() + " has finished!");
+    }
+  }
 }
